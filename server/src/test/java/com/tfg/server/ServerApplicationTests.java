2a590ce5e4c8d22ee017615b1b4af9792215983a
Francesc <elskater98@gmail.com>
2020-03-16 20:54:05 +0100

    Inital spring enviorement

---
+++ server/src/test/java/com/tfg/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
