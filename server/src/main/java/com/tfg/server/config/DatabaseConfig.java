244300d76c8cadfe77953a4802622d1912754e0a
Francesc <elskater98@gmail.com>
2020-03-26 23:08:16 +0100

    Heroku Deployment

--- server/src/main/java/com/tfg/server/config/DatabaseConfig.java
+++ server/src/main/java/com/tfg/server/config/DatabaseConfig.java
@@ -1,20 +1,32 @@
 package com.tfg.server.config;
 
-import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
-import javax.sql.DataSource;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by http://rhizomik.net/~roberto/
+ */
 @Configuration
+@Profile("heroku")
 public class DatabaseConfig {
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
     @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        DataSource basicDataSource = new DataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 }
